b6e6fdf5169183845fbe2e1090652886f6996026
wagner <wagner.franca@fatec.sp.gov.br>
2021-04-03 12:37:57 -0300

    Udemy - Algoritmos e Lógica

--- src/vendas.java
+++ src/vendas.java
@@ -1,4 +1,7 @@
 /*
+*
+* Wagner de Souza França - FATEC
+*
 *    Uma Empresa de vendas de softwares paga a seu vendedor um fixo de
 *    R$ 800,00 por mês, mais uma comissão de 15% pelo seu valor de vendas no mês. Faça
 *    uma algoritmo que leia o valor da venda e determine o salário total do funcionário.
